02a5845afcffd434ba35fa33b8d0db694d62618d
4ug-aug <augusttollerup@hotmail.com>
2021-10-25 11:19:37 +0200

    Refactoring

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # start-server.sh
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd tømmerbob; python manage.py createsuperuser --no-input)
+    (cd tommerbob; python manage.py createsuperuser --no-input)
 fi
-(cd tømmerbob; gunicorn tømmerbob.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd tommerbob; gunicorn tømmerbob.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
