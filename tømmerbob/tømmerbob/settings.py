9113429e8f0303d8e462c3bdf0a721d482723d62
4ug-aug <augusttollerup@hotmail.com>
2021-10-25 10:29:21 +0200

    Minor refactoring

--- tømmerbob/tømmerbob/settings.py
+++ tømmerbob/tømmerbob/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +128,8 @@ USE_TZ = True
 STATIC_URL = 'polls/static/'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
